8f3e2784f5c13919fbbd36e883ba774124297469
sfrancis <sfrancis@25c0949a-662f-0410-90fc-d2c3c67ea922>
2002-07-24 21:26:34 +0000

    Skeleton test of the HProfParser

---
+++ trunk/prophit/test/java/orbit/parsers/TestHProfParser.java
@@ -0,0 +1,35 @@
+package orbit.parsers;
+
+import orbit.model.*;
+import orbit.parsers.HProfParser;
+
+import junit.framework.TestCase;
+
+import java.util.*;
+import java.io.*;
+
+public class TestHProfParser
+	extends TestCase
+{
+	public TestHProfParser(String name)
+	{
+		super(name);
+	}
+
+	public void testParse() throws Exception
+	{
+		File file = new File(System.getProperty("basedir") + "/test/data/hello.hprof.txt");
+		
+		HProfParser parser = new HProfParser(new FileReader(file));
+		parser.execute();
+
+		List callIDs = parser.getCallIDs();
+
+		System.out.println(callIDs);
+	}
+
+	static CallID callID(List callIDs, int index)
+	{
+		return (CallID)callIDs.get(index);
+	}
+}
