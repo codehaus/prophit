2df1888b50ece9dfc88a945fc5b3ee384327eb1c
sfrancis <sfrancis@25c0949a-662f-0410-90fc-d2c3c67ea922>
2002-09-11 21:10:03 +0000

    Implemented sample profiles

---
+++ trunk/prophit/java/orbit/gui/SampleProfileType.java
@@ -0,0 +1,31 @@
+package orbit.gui;
+
+import java.util.*;
+
+class SampleProfileType
+{
+	private final String name;
+	private final ArrayList profiles = new ArrayList();
+	
+	public SampleProfileType(String name)
+	{
+		this.name = name;
+	}
+
+	public String getName() { return name; }
+
+	public void addProfile(SampleProfile profile)
+	{
+		profiles.add(profile);
+	}
+
+	public List getProfiles()
+	{
+		return Collections.unmodifiableList(profiles);
+	}
+
+	public String toString()
+	{
+		return name + "[ " + profiles.size() + " profiles ]";
+	}
+}
