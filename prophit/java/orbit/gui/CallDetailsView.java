9ec79fe7534b6ddd450844cf5d3709fe17fea932
sfrancis <sfrancis@25c0949a-662f-0410-90fc-d2c3c67ea922>
2002-10-09 15:21:09 +0000

    Changed to use a more standard Model/view architecture, using BlockDiagramModel

--- prophit/java/orbit/gui/CallDetailsView.java
+++ prophit/java/orbit/gui/CallDetailsView.java
@@ -7,16 +7,42 @@ import java.awt.Dimension;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
+import java.beans.PropertyChangeSupport;
+import java.beans.PropertyChangeEvent;
+import java.beans.PropertyChangeListener;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.JSplitPane;
 import javax.swing.JTable;
 import javax.swing.ListSelectionModel;
 import javax.swing.table.AbstractTableModel;
+import javax.swing.table.DefaultTableModel;
 
-abstract class CallDetailsView
+/**
+ * Contains the detailed information about the selected call. This includes the time and numCalls information for the
+ * selected call, as well as the tables of callers and callees.
+ * <p>
+ * This class uses the {@link BlockDiagramModel} as its model. When a new profile is loaded, {@link #setModel} can be used
+ * to update the CallDetailsView.
+ * <p>
+ * This class fires PropertyChangeEvents when a caller or callee is double-clicked by the user.
+ *
+ * @see #addListener
+ */
+class CallDetailsView
 	extends JPanel
 {
+	/**
+	 * PropertyChangeEvent fired when the user double-clicks or otherwise selects a row in the callers table.
+	 * @see #addListener
+	 */
+	public static final String SELECTED_CALLER_PROPERTY = "selectedCaller";
+	/**
+	 * PropertyChangeEvent fired when the user double-clicks or otherwise selects a row in the callees table.
+	 * @see #addListener
+	 */
+	public static final String SELECTED_CALLEE_PROPERTY = "selectedCallee";
+
 	public static final int MINIMUM_WIDTH = 150;
 	public static final int MINIMUM_HEIGHT = 100;
 	public static final int PREFERRED_WIDTH = 300;
@@ -25,32 +51,82 @@ abstract class CallDetailsView
 	private JTable tblCallInfo;
 	private JTable tblCallers;
 	private JTable tblCallees;
+	private BlockDiagramModel blockModel;
+	private PropertyChangeSupport changeSupport;
 
-	public CallDetailsView()
+	public CallDetailsView(BlockDiagramModel blockModel)
 	{
 		super(new BorderLayout());
 
+		changeSupport = new PropertyChangeSupport(this);
+		
 		addComponents();
+
+		setModel(blockModel);
 	}
 
-	public abstract void callerSelected(String callerName);
-
-	public abstract void calleeSelected(String calleeName);
-
-	public void selectedCallChanged(Call rootCall, Call selectedCall)
+	public void setModel(BlockDiagramModel blockModel)
 	{
-		// Might as well leave it alone if the user clicks empty space
+		this.blockModel = blockModel;
+		this.blockModel.addListener(new PropertyChangeListener()
+			{
+				public void propertyChange(PropertyChangeEvent evt)
+				{
+					if ( BlockDiagramModel.RENDER_CALL_PROPERTY.equals(evt.getPropertyName()) ||
+						 BlockDiagramModel.SELECTED_CALL_PROPERTY.equals(evt.getPropertyName()) )
+					{
+						selectedCallChanged();
+					}
+				}
+			});
+
+		selectedCallChanged();
+	}		
+
+	/**
+	 * @see #removeListener
+	 */
+	public synchronized void addListener(PropertyChangeListener listener)
+	{
+		changeSupport.addPropertyChangeListener(listener);
+	}
+	
+	/**
+	 * @see #addListener
+	 */
+	public synchronized void removeListener(PropertyChangeListener listener)
+	{
+		changeSupport.removePropertyChangeListener(listener);
+	}
+
+	/**
+	 * Invoke this method when the selected call is changed. It can be changed by the user selecting
+	 * a new block with the mouse, or by navigating through the callers and callees by double-clicking on
+	 * the corresponding tables, when the diagram is drawn with a new root render call, or when a new
+	 * profile is loaded.
+	 */
+	public void selectedCallChanged()
+	{
+		Call rootCall = blockModel.getRootRenderState().getRenderCall();
+		Call selectedCall = blockModel.getSelectedCall();
+
 		if ( selectedCall != null )
 		{
 			CallDetails details = new CallDetails(rootCall, selectedCall);
-			
+		
 			tblCallInfo.setModel(new CallInfoModel(details));
 			tblCallInfo.getColumnModel().getColumn(0).setPreferredWidth(40);
-
+		
 			tblCallers.setModel(details.getCallersModel());
 			tblCallers.getColumnModel().getColumn(1).setPreferredWidth(40);
 			tblCallees.setModel(details.getCalleesModel());
 			tblCallees.getColumnModel().getColumn(1).setPreferredWidth(40);
+		}
+		else
+		{
+			tblCallInfo.setModel(new DefaultTableModel());
+			tblCallers.setModel(new DefaultTableModel());
+			tblCallees.setModel(new DefaultTableModel());
 		}
 	}
 
@@ -86,7 +162,8 @@ abstract class CallDetailsView
 					{
 						int row = tblCallers.rowAtPoint(e.getPoint());
 						// System.out.println("Double-clicked " + row);
-						callerSelected(((CallDetails.CallTableModel)tblCallers.getModel()).getCallName(row));
+						String callName = ((CallDetails.CallTableModel)tblCallers.getModel()).getCallName(row);
+						changeSupport.firePropertyChange(SELECTED_CALLER_PROPERTY, null, callName);
 					}
 				}
 			});
@@ -100,7 +177,8 @@ abstract class CallDetailsView
 					{
 						int row = tblCallees.rowAtPoint(e.getPoint());
 						// System.out.println("Double-clicked " + row);
-						calleeSelected(((CallDetails.CallTableModel)tblCallees.getModel()).getCallName(row));
+						String callName = ((CallDetails.CallTableModel)tblCallees.getModel()).getCallName(row);
+						changeSupport.firePropertyChange(SELECTED_CALLEE_PROPERTY, null, callName);
 					}
 				}
 			});
@@ -127,13 +205,6 @@ abstract class CallDetailsView
 		scrollInfo.setPreferredSize(preferred);
 		
 		add(topSplitter, BorderLayout.CENTER);
-
-		/*
-		bottomSplitter.resetToPreferredSizes();
-		topSplitter.resetToPreferredSizes();
-		topSplitter.setDividerLocation(0.333);
-		bottomSplitter.setDividerLocation(0.5);
-		*/
 	}
 
 	private class CallInfoModel
