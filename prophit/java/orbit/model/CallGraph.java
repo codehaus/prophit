3e11378bcc2afd65e1187a236168920ca9f9b41f
sfrancis <sfrancis@25c0949a-662f-0410-90fc-d2c3c67ea922>
2002-11-01 15:39:48 +0000

    Automatically adds a root call <root>. Does not implement Call any more. Added getRoot method. Implemented getMaxDepth & other new Call interface functions in FilteredCallImpl

--- prophit/java/orbit/model/CallGraph.java
+++ prophit/java/orbit/model/CallGraph.java
@@ -12,14 +12,18 @@ import java.util.List;
  * in the CallGraph implement the {@link Call} interface, which is a simple but complete representation.
  */
 public class CallGraph
-	implements Call
 {
 	private static final String LINE_SEPARATOR = System.getProperty("line.separator");
 	
-	private final CallID[] callIDs;
-	private final int[] parentRCCKeys;
+	private final CallID     rootCallID;
+	private final CallID[]   callIDs;
+	private final int[]      parentRCCKeys;
 	private final IntStack[] childRCCKeys;
-	private final double[] callFractions;
+	private final double[]   callFractions;
+	private final CallImpl   rootCall;
+
+	// This is only calculated on demand
+	private int maxDepth = -1;
 
 	/**
 	 * @see #CallGraph(CallID[], double[])
@@ -39,27 +43,84 @@ public class CallGraph
 	 */
 	public CallGraph(CallID[] callIDs, double[] callFractions)
 	{
-		this.callIDs = callIDs;
-		this.callFractions = callFractions;
+		if ( callIDs.length == 0 )
+			throw new IllegalArgumentException("Must be at least 1 callID");
+		if ( callIDs.length != callFractions.length )
+			throw new IllegalArgumentException("Length of callIDs and callFractions arrays should be the same");
+
+		// Compute the total time spent by all the top-level functions
+		// Look for a unique top-level callID
+		long rootTime = 0;
+		ArrayList rootIDs = new ArrayList();
+		for ( int i = 0; i < callIDs.length; ++i )
+		{
+			CallID id = callIDs[i];
+			if ( id != null && id.getParentRCCKey() == -1 )
+			{
+				rootIDs.add(id);
+				rootTime += id.getRCC().getTime();
+			}
+		}
+
+		if ( rootIDs.size() == 0 )
+		{
+			throw new IllegalArgumentException("Must be at least one root callID");
+		}
+		else if ( rootIDs.size() == 1 )
+		{
+			rootCallID = (CallID)rootIDs.get(0);
+			this.callIDs = callIDs;
+			this.callFractions = callFractions;
+		}
+		else
+		{
+			/*
+			 * Construct a new CallID which will represent the root of the CallGraph
+			 * Make it the parent of all the root CallIDs
+			 * Add it to the end of the callIDs and callFractions arrays
+			 */
+			int numRCCs = callIDs.length + 1;
+			this.callIDs = new CallID[numRCCs];
+			this.callFractions = new double[numRCCs];
+
+			RCC rootRCC = new RCC(new StackTrace(new String[0]), 1, rootTime, callIDs.length);
+			rootCallID = new CallID(rootRCC, null);
+			for ( Iterator i = rootIDs.iterator(); i.hasNext(); )
+			{
+				CallID callID = (CallID)i.next();
+				callID.setParent(rootRCC);
+			}
+			
+			System.arraycopy(callIDs, 0, this.callIDs, 0, callIDs.length);
+			System.arraycopy(callFractions, 0, this.callFractions, 0, callFractions.length);
+			
+			this.callIDs[rootCallID.getKey()] = rootCallID;
+			this.callFractions[rootCallID.getKey()] = 1;
+		}
+		rootCall = new CallImpl(rootCallID);
 		
-		parentRCCKeys = new int[this.callIDs.length];
-		childRCCKeys = new IntStack[this.callFractions.length];
+		int numRCCs = this.callIDs.length;
+		parentRCCKeys = new int[numRCCs];
+		childRCCKeys = new IntStack[numRCCs];
+		
 		for ( int i = 0; i < this.callIDs.length; ++i )
 		{
 			CallID id = this.callIDs[i];
 			// Gaps in the callIDs array are filled in later by proxy calls
 			if ( id != null )
 			{
+				int key = id.getKey();
 				int parentKey = id.getParentRCCKey();
 				parentRCCKeys[id.getKey()] = parentKey;
-				if ( parentKey > 0 )
+				if ( parentKey != -1 )
 				{
 					if ( childRCCKeys[parentKey] == null )
 						childRCCKeys[parentKey] = new IntStack();
-					childRCCKeys[parentKey].push(id.getKey());
+					childRCCKeys[parentKey].push(key);
 				}
 			}
 		}
+		
 		for ( int i = 0; i < childRCCKeys.length; ++i )
 		{
 			if ( childRCCKeys[i] != null )
@@ -67,118 +128,60 @@ public class CallGraph
 		}
 	}
 
-	/** @return a key which is greater than the maximum key of any Call in the CallGraph. */
-	public int getKey() { return callIDs.length; }
-	/** @return "<root>" */
-	public String getName() { return "<root>"; }
-	public int getDepth() { return 0; }
-	public int getCallCount() { return 1; }
+	public CallID[] getCallIDs() { return callIDs; }
 
-	public double getTime()
-	{
-		double time = 0;
-		for ( Iterator i = getChildren().iterator(); i.hasNext(); )
-		{
-			Call call = (Call)i.next();
-			time += call.getTime();
-		}
-		return time;
-	}
-
-	public Call getParent() { return null; }
+	public double[] getFractions() { return callFractions; }
 	
-	public List getChildren()
+	public Call getRoot()
 	{
-		ArrayList roots = new ArrayList();
-		for ( int i = 0; i < parentRCCKeys.length; ++i )
-		{
-			if ( parentRCCKeys[i] == -1 )
-				roots.add(new CallImpl(callIDs[i]));
-		}
-		return roots;
-	}
-
-	public void depthFirstTraverse(Visitor visitor)
-	{
-		for ( Iterator i = getChildren().iterator(); i.hasNext(); )
-		{
-			CallImpl root = (CallImpl)i.next();
-			depthFirstTraverse(root.id, new IntStack(), visitor);
-		}
+		return rootCall;
 	}
 	
+	public void setMaxDepth(int maxDepth)
+	{
+		if ( this.maxDepth != -1 &&
+			 this.maxDepth != maxDepth )
+		{
+			throw new IllegalArgumentException("CallGraph.maxDepth was already determined to be " + this.maxDepth +
+											   ", and now it is being set to " + maxDepth);
+		}
+		this.maxDepth = maxDepth;
+	}
+
+	public int getMaxDepth()
+	{
+		if ( maxDepth == -1 )
+		{
+			MaxDepthVisitor visitor = new MaxDepthVisitor(-1);
+			getRoot().depthFirstTraverse(visitor);
+			maxDepth = visitor.getMaxDepth();
+		}
+		return maxDepth;
+	}
+
 	public String toString()
 	{
 		StringBuffer sb = new StringBuffer();
-		sb.append(getName());sb.append(" { numInvocations : ");sb.append(getCallCount());
-		sb.append(", timeMillis : ");sb.append(getTime());sb.append(" }");
+		sb.append("CallGraph [ root = ");sb.append(rootCallID);
+		sb.append(", maxDepth = ");sb.append(maxDepth);sb.append(" ]");
 		return sb.toString();
 	}
 		
 	public String toString(int maxDepth)
 	{
-		StringBuffer sb = new StringBuffer();
-		for ( Iterator i = getChildren().iterator(); i.hasNext(); )
-		{
-			CallImpl root = (CallImpl)i.next();
-			toString(root, sb, 0, maxDepth);
-		}
-		return sb.toString();
+		CallImpl root = (CallImpl)getRoot();
+		return root.toString(maxDepth);
 	}
 
-	private void depthFirstTraverse(CallID id, IntStack stack, Visitor visitor)
-	{
-		stack.push(id.getKey());
-		if ( visitor.visit(id, stack) )
-		{
-			IntStack childKeys = childRCCKeys[id.getRCC().getKey()];
-			if ( childKeys != null )
-			{
-				for ( IntIterator i = childKeys.iterator(); i.hasNext(); )
-				{
-					int childKey = i.next();
-					if ( !stack.contains(childKey) )
-					{
-						CallID child = callIDs[childKey];
-						depthFirstTraverse(child, stack, visitor);
-					}
-				}
-			}
-		}
-		stack.pop();
-	}
-	
-	private void toString(CallImpl call, StringBuffer sb, int depth, int maxDepth)
-	{
-		if ( maxDepth != -1 && depth > maxDepth )
-			return;
-		
-		for ( int i = 0; i < depth; ++i )
-			sb.append("  ");
-		sb.append(call.getName());sb.append(" ");sb.append(call.getTime());sb.append(" [");sb.append(call.getParentKey());sb.append("->");sb.append(call.getKey());sb.append("]");
-		sb.append(LINE_SEPARATOR);
-		for ( Iterator i = call.getChildren().iterator(); i.hasNext(); )
-		{
-			CallImpl child = (CallImpl)i.next();
-			toString(child, sb, depth + 1, maxDepth);
-		}
-	}
-
-	public interface Visitor
-	{
-		/**
-		 * @return true if the Visitor wants to continue visiting the children of <code>callID</code>.
-		 */
-		public boolean visit(CallID callID, IntStack callStack);
-	}
-	
-	public class CallImpl
+	class CallImpl
 		implements Call
 	{
-		private final CallID id;
-		private final IntStack stack;
-		private int callCount = -1;
-		private double time = -1;
+		protected final CallID id;
+		protected final IntStack stack;
+		
+		private int    callCount = -1;
+		private int    maxDepth  = -1;
+		private double time      = -1;
 
 		private CallImpl(CallID id)
 		{
@@ -213,30 +216,36 @@ public class CallGraph
 			return stack.hashCode();
 		}
 
-		private CallImpl createChild(CallID id)
-		{
-			IntStack copy = (IntStack)stack.clone();
-			copy.push(id.getKey());
-			return new CallImpl(copy, id);
-		}
-
-		private Call createParent()
-		{
-			if ( stack.size() == 1 )
-				return CallGraph.this;
-			IntStack copy = (IntStack)stack.clone();
-			copy.pop();
-			return new CallImpl(copy, callIDs[copy.top()]);
-		}
-		
 		public int getDepth()
 		{
 			return stack.size();
 		}
-
+		
+		public synchronized int getMaxDepth()
+		{
+			if ( maxDepth == -1 )
+			{
+				if ( getParent() == null && !hasFilter() )
+				{
+					maxDepth = CallGraph.this.getMaxDepth();
+				}
+				else
+				{
+					MaxDepthVisitor visitor = new MaxDepthVisitor(-1);
+					depthFirstTraverse(visitor);
+					maxDepth = visitor.getMaxDepth();
+				}
+			}
+			return maxDepth;
+		}
+		
 		public String getName()
 		{
-			return id.getRCC().getLeafMethodName();
+			StackTrace st = id.getRCC().getStack();
+			if ( st.size() > 0 )
+				return id.getRCC().getLeafMethodName();
+			else
+				return "<root>";
 		}
 
 		public synchronized int getCallCount()
@@ -263,7 +272,17 @@ public class CallGraph
 		{
 			return createParent();
 		}
-	
+
+		public Call filter(Filter filter)
+		{
+			return new FilteredCallImpl(stack, id, this, filter);
+		}
+
+		public void depthFirstTraverse(Visitor visitor)
+		{
+			depthFirstTraverse(id, new IntStack(), visitor);
+		}
+		
 		public List getChildren()
 		{
 			// System.out.println("Children of " + id);
@@ -276,8 +295,13 @@ public class CallGraph
 					int childKey = i.next();
 					if ( !stack.contains(childKey) )
 					{
-						// System.out.println("\tAdding child " + callIDs[childKey]);
-						children.add(createChild(callIDs[childKey]));
+
+						CallImpl child = createChild(callIDs[childKey]);
+						if ( addChild(child) )
+						{
+							// System.out.println("\tAdding child " + callIDs[childKey]);
+							children.add(child);
+						}
 					}
 				}
 			}
@@ -297,12 +321,97 @@ public class CallGraph
 				return -1;
 		}
 		
+		public String toString(int maxDepth)
+		{
+			StringBuffer sb = new StringBuffer();
+			toString(sb, 0, maxDepth);
+			return sb.toString();
+		}
+
 		public String toString()
 		{
 			StringBuffer sb = new StringBuffer();
 			sb.append(getName());sb.append(" { numInvocations : ");sb.append(getCallCount());
 			sb.append(", timeMillis : ");sb.append(getTime());sb.append(", callStack : ");sb.append(stack);sb.append(" }");
 			return sb.toString();
+		}
+
+		protected boolean hasFilter()
+		{
+			return true;
+		}
+		
+		protected boolean addChild(CallImpl child)
+		{
+			return true;
+		}
+		
+		protected boolean doTraverse(CallID id, IntStack stack)
+		{
+			return true;
+		}
+		
+		protected CallImpl createChild(CallID id)
+		{
+			IntStack copy = (IntStack)stack.clone();
+			copy.push(id.getKey());
+			return new CallImpl(copy, id);
+		}
+
+		protected Call createParent()
+		{
+			if ( stack.size() == 1 )
+				return null;
+			IntStack copy = (IntStack)stack.clone();
+			copy.pop();
+			return new CallImpl(copy, callIDs[copy.top()]);
+		}
+		
+		private void depthFirstTraverse(CallID id, IntStack stack, Visitor visitor)
+		{
+			stack.push(id.getKey());
+			try
+			{
+				if ( doTraverse(id, stack) )
+				{
+					if ( visitor.visit(id, stack) )
+					{
+						IntStack childKeys = childRCCKeys[id.getRCC().getKey()];
+						if ( childKeys != null )
+						{
+							for ( IntIterator i = childKeys.iterator(); i.hasNext(); )
+							{
+								int childKey = i.next();
+								if ( !stack.contains(childKey) )
+								{
+									CallID child = callIDs[childKey];
+									depthFirstTraverse(child, stack, visitor);
+								}
+							}
+						}
+					}
+				}
+			}
+			finally
+			{
+				stack.pop();
+			}
+		}
+	
+		private void toString(StringBuffer sb, int depth, int maxDepth)
+		{
+			if ( maxDepth != -1 && depth > maxDepth )
+				return;
+			
+			for ( int i = 0; i < depth; ++i )
+				sb.append("  ");
+			sb.append(getName());sb.append(" ");sb.append(getTime());sb.append(" [");sb.append(getParentKey());sb.append("->");sb.append(getKey());sb.append("]");
+			sb.append(LINE_SEPARATOR);
+			for ( Iterator i = getChildren().iterator(); i.hasNext(); )
+			{
+				CallImpl child = (CallImpl)i.next();
+				child.toString(sb, depth + 1, maxDepth);
+			}
 		}
 		
 		private int getRCCKey()
@@ -319,6 +428,73 @@ public class CallGraph
 				multiplier *= callFractions[key];
 			}
 			return multiplier;
+		}
+	}
+
+	class FilteredCallImpl
+		extends CallImpl
+	{
+		private final CallImpl root;
+		private final Filter   filter;
+		
+		private FilteredCallImpl(IntStack stack, CallID id, CallImpl root, Filter filter)
+		{
+			super(stack, id);
+
+			this.root = root;
+			this.filter = filter;
+		}
+
+		public boolean equals(Object other)
+		{
+			if ( !( other instanceof FilteredCallImpl ) )
+				return false;
+			return equals((FilteredCallImpl)other);
+		}
+
+		public boolean equals(FilteredCallImpl other)
+		{
+			if ( !super.equals(other) )
+				return false;
+			return root.equals(other.root) && filter.equals(other.filter);
+		}
+		
+		protected boolean doTraverse(CallID id, IntStack stack)
+		{
+			CallImpl call = new CallImpl(stack, id);
+			return filter.execute(root, call);
+		}
+		
+		protected boolean hasFilter()
+		{
+			return false;
+		}
+		
+		protected boolean addChild(CallImpl child)
+		{
+			return filter.execute(root, child);
+		}
+		
+		protected CallImpl createChild(CallID id)
+		{
+			IntStack copy = (IntStack)stack.clone();
+			copy.push(id.getKey());
+			return new FilteredCallImpl(copy, id, root, filter);
+		}
+
+		protected Call createParent()
+		{
+			if ( stack.size() == 1 )
+				return null;
+			IntStack copy = (IntStack)stack.clone();
+			copy.pop();
+			/*
+			if ( copy.size() < root.getDepth() )
+			{
+				throw new RuntimeException("It is illegal to get the parent of a root FilteredCallImpl");
+			}
+			*/
+			return new FilteredCallImpl(copy, callIDs[copy.top()], root, filter);
 		}
 	}
 }
