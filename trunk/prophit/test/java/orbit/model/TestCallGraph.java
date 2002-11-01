16ab9845fe9c20f53423425cdf68c84b0a0ab6b2
sfrancis <sfrancis@25c0949a-662f-0410-90fc-d2c3c67ea922>
2002-11-01 15:43:27 +0000

    Added tests for Call.filter

--- trunk/prophit/test/java/orbit/model/TestCallGraph.java
+++ trunk/prophit/test/java/orbit/model/TestCallGraph.java
@@ -21,7 +21,7 @@ public class TestCallGraph
 
 	public void testSimpleModel() throws Exception
 	{
-		Call main = (Call)simpleCG.cg.getChildren().get(0);
+		Call main = (Call)simpleCG.cg.getRoot();
 		Call test = (Call)main.getChildren().get(0);
 		Call DBExec1 = (Call)test.getChildren().get(0);
 
@@ -32,5 +32,62 @@ public class TestCallGraph
 
 		DBExec1 = (Call)simpleCG.test.getChildren().get(0);
 		assertEquals(DBExec1, simpleCG.DBExec1);
+		
+		assertEquals(simpleCG.cg.getMaxDepth(), 5);
+	}
+
+	public void testFilter() throws Exception
+	{
+		// Filter out all calls which take less than 10% of the time
+		Call.Filter filter;
+
+		// Apply the filter to the main() function (the root function)
+		// Filter out everything
+		filter = new InclusiveTimeFilter(2.0);
+		Call main = simpleCG.main.filter(filter);
+
+		assertEquals(simpleCG.main.filter(filter), simpleCG.main.filter(filter));
+		// Would be better if this was able to ascertain that the filtered call is not the same
+		//   as the unfiltered call
+		assertTrue(simpleCG.main.equals(simpleCG.main.filter(filter)));
+		// It works if you call FilteredCallImpl.equals() though
+		assertTrue(!simpleCG.main.filter(filter).equals(simpleCG.main));
+		assertEquals(main.getChildren().size(), 0);
+		assertEquals(main.getParent(), null);
+
+		// Filter out the 2nd DBExec but not the first
+		filter = new InclusiveTimeFilter(0.35);
+		main = simpleCG.main.filter(filter);
+		assertEquals(main.getParent(), null);
+		assertEquals(main.getChildren().size(), 2);
+		
+		Call test = (Call)main.getChildren().get(0);
+		assertEquals(test.getName(), "test");
+		assertEquals(test.getTime(), 180, 0);
+		assertEquals(test.getChildren().size(), 1);
+		assertEquals(test.getChildren().get(0), test.getChildren().get(0));
+		
+		Call DBExec = (Call)test.getChildren().get(0);
+		assertEquals(DBExec.getName(), "DBExec");
+		assertEquals(DBExec.getTime(), 120, 0);
+		assertEquals(DBExec.getChildren().size(), 0);
+
+		assertEquals(DBExec.getParent(), test);
+		assertEquals(DBExec.getParent().getParent(), main);
+		assertEquals(DBExec.getParent().getParent().getParent(), null);
+
+		/*
+		boolean exception = false;
+		try
+		{
+			// Can't get the parent of the root-level filtered Call
+			simpleCG.test.filter(filter).getParent();
+		}
+		catch (RuntimeException x)
+		{
+			exception = true;
+		}
+		assertTrue(exception);
+		*/
 	}
 }
