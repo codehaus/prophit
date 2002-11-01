6b45fe659fd9d24d934ebc8aebd78a6bc1c88d26
sfrancis <sfrancis@25c0949a-662f-0410-90fc-d2c3c67ea922>
2002-11-01 15:31:54 +0000

    Added drawText

--- trunk/prophit/java/orbit/gui/GLUtils.java
+++ trunk/prophit/java/orbit/gui/GLUtils.java
@@ -1,11 +1,24 @@
 package orbit.gui;
 
 import gl4java.GLFunc;
+import gl4java.utils.glut.GLUTEnum;
+import gl4java.utils.glut.GLUTFunc;
 
 import java.awt.Color;
 
 public class GLUtils
 {
+	/**
+	 * Assumes that the modelview and projection matrices have been set up already, probably to
+	 * an orthogonal projection
+	 */
+	public static void drawText(GLFunc gl, GLUTFunc glut, int x, int y, String text, Color color)
+	{
+		GLUtils.glColor(gl, color);
+		gl.glRasterPos2i(x, y);
+		glut.glutBitmapString(GLUTEnum.GLUT_BITMAP_HELVETICA_12, text);
+	}
+
 	public static void glColor(GLFunc gl, Color color)
 	{
 		gl.glColor4f(( color.getRed() / 255.0f ), ( color.getGreen() / 255.0f ),
