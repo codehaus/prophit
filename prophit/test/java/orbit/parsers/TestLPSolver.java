9bad0ccb83fc5838a6d0803ae86d913317dc9df5
sfrancis <sfrancis@25c0949a-662f-0410-90fc-d2c3c67ea922>
2002-11-25 17:46:18 +0000

    Added ModelBuilder to test code. Tests all call fraction results

--- prophit/test/java/orbit/parsers/TestLPSolver.java
+++ prophit/test/java/orbit/parsers/TestLPSolver.java
@@ -22,22 +22,41 @@ public class TestLPSolver
 	{
 		File file = new File(System.getProperty("basedir") + "/data/simple.prof");
 		DashProfParser parser = new DashProfParser(new FileReader(file));
-		parser.execute();
-		List callIDs = parser.getCallIDs();
+		ModelBuilder builder = ModelBuilderFactory.newModelBuilder();
+		parser.execute(builder);
+
+		HashMap callIDsByKey = new HashMap();
+		
+		List callIDs = builder.getCallIDs();
+		for ( Iterator i = callIDs.iterator(); i.hasNext(); )
+		{
+			CallID callID = (CallID)i.next();
+			if ( callID != null )
+				callIDsByKey.put(new Integer(callID.getKey()), callID);
+		}
+
+		// Should not be any CallID with key = 0
+		assertEquals(callIDsByKey.get(new Integer(0)), null);
+		
 		LPSolver solver = new LPSolver();
 		solver.solve(callIDs);
 		double[] fractions = solver.getFractions();
 
-		assertEquals(14, fractions.length);
-		assertEquals(1, fractions[0], TestUtil.TOLERANCE);
-		assertEquals(1, fractions[1], TestUtil.TOLERANCE);
-		assertEquals(0.333333, fractions[10], TestUtil.TOLERANCE);
-		assertEquals(0.666667, fractions[11], TestUtil.TOLERANCE);
 		/*
 		for ( int i = 0; i < fractions.length; ++i )
 		{
-			System.out.println(i + " = " + fractions[i]);
+			System.out.println(callIDsByKey.get(new Integer(i)) + ".fraction = " + fractions[i]);
 		}
 		*/
+		
+		assertEquals(14, fractions.length);
+		for ( int i = 0; i < 10; ++i )
+		{
+			assertEquals(1, fractions[i], TestUtil.TOLERANCE);
+		}
+		assertEquals(0.333333, fractions[10], TestUtil.TOLERANCE);
+		assertEquals(0.666667, fractions[11], TestUtil.TOLERANCE);
+		assertEquals(0.333333, fractions[12], TestUtil.TOLERANCE);
+		assertEquals(0.666667, fractions[13], TestUtil.TOLERANCE);
 	}
 }
